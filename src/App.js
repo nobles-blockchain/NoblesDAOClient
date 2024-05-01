6740186b8c26e407bd9e02b00c0859c94193a13b
Altan Marvi <marvi36090@Altans-MacBook-Air.local>
2024-05-01 11:35:55 -0400

    VotingPage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-
+import Voting_Page from "./Voting_Page/votingpage"
 function App() {
   return (
     <div>
-      CREATE REACT
+        <Voting_Page></Voting_Page>
     </div>
+    
+    
+
+    
+
+    
+
   );
 }
 
 export default App;
+
+//allow people to vote using buttons
