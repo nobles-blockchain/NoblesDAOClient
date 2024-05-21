f4c092767440ddb7065911fd822219b821cb0417
SwiftlyNoah <devnoahapps@gmail.com>
2024-05-21 12:35:20 -0400

    Fix login

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import CreateAccount from "./Components/CreateAccount/CreateAccount";
 import HomePage from "./Components/Homepage/Homepage";
@@ -9,19 +9,19 @@ import PollPage from "./Components/PollPage/PollPage";
 import { getUserIdFromCookie } from './Components/Cookies/AuthServices.js';
 
 function App() {
-  const checkLogin = () => !!getUserIdFromCookie();
+  const [isLoggedIn, setIsLoggedIn] = useState(!!getUserIdFromCookie());
 
   return (
     <Router>
       <div>
         <Routes>
-          <Route path="/" element={<Navigate to={checkLogin() ? "/home-page" : "/login"} />} />
+          <Route path="/" element={<Navigate to={isLoggedIn ? "/home-page" : "/login"} />} />
           <Route path="/create-account" element={<CreateAccount />} />
-          <Route path="/home-page" element={checkLogin() ? <HomePage /> : <Navigate to="/login" />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/create-poll" element={checkLogin() ? <CreatePoll /> : <Navigate to="/login" />} />
+          <Route path="/home-page" element={isLoggedIn ? <HomePage /> : <Navigate to="/login" />} />
+          <Route path="/login" element={<LoginPage onLogin={() => setIsLoggedIn(true)} />} />
+          <Route path="/create-poll" element={isLoggedIn ? <CreatePoll /> : <Navigate to="/login" />} />
           <Route path="/vote" element={<VotingPage />} />
-          <Route path="/poll/:pollId" element={checkLogin() ? <PollPage /> : <Navigate to="/login" />} />
+          <Route path="/poll/:pollId" element={isLoggedIn ? <PollPage /> : <Navigate to="/login" />} />
         </Routes>
       </div>
     </Router>
